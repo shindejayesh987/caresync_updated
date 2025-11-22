2b7e83e17d5c2e09989287748eb9ebb8dbc48565
Jayesh Shinde <64770565+shindejayesh987@users.noreply.github.com>
2025-11-22 17:17:27 -0500

    Update main entrypoint and Dockerfile

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,9 @@
-from __future__ import annotations
-
 import os
-
 import uvicorn
-
-from backend.app import create_app
+from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":  # pragma: no cover
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
